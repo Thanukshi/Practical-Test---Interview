384ade286eb57e6ba2566812b858e6f169ef72ea
Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2023-03-14 16:59:32 +0530

    Add Common API URL

---
+++ client/src/Data/API.js
@@ -0,0 +1,8 @@
+export const API_URL = 'https://localhost:7249'; 
+
+//Subject Urls
+// export const SUBJECT_ADD_URL = "/Subject/AddSubject";
+// export const SUBJECT_GET_URL = "/Subject/GetSubjectList";
+// export const SUBJECT_GET_ID_URL = "/Subject/GetSubjectById/2";
+// export const SUBJECT_REMOVE_URL = "/api/subjects";
+// export const SUBJECT_UPDATE_URL = "/api/subjects";
